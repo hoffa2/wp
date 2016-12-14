b43d7cd2fdb43ce51f5d30a02acdc862bc2c511e
hoffa2 <helhof@online.no>
2016-12-14 09:36:53 +0100

    test: added concurrent add test

--- pool_test.go
+++ pool_test.go
@@ -1,9 +1,6 @@
 package wp
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 type test struct{}
 
@@ -13,7 +10,7 @@ func TestInit(t *testing.T) {
 	p := NewPool(100, nil, func(args interface{}) error {
 		s := args.(chan int)
 		s <- 0
-		return fmt.Errorf("lol")
+		return nil
 	})
 	p.Start()
 	for i := 0; i < 10000; i++ {
@@ -38,4 +35,43 @@ func TestInit(t *testing.T) {
 		t.Errorf("Should have %d - got %d", tests, receives)
 	}
 	p.Quit()
+}
+
+func TestConcurrentAdd(t *testing.T) {
+	assertChan := make(chan int, 10000)
+	tests := 10000
+	p := NewPool(100, nil, func(args interface{}) error {
+		s := args.(chan int)
+		s <- 0
+		return nil
+	})
+
+	p.Start()
+
+	for i := 0; i < 10000; i++ {
+		go func(i int) {
+			p.Add(assertChan)
+		}(i)
+	}
+	p.Wait()
+
+	receives := 0
+	done := false
+	for {
+		select {
+		case <-assertChan:
+			receives++
+		default:
+			done = true
+			break
+		}
+		if done {
+			break
+		}
+	}
+	if receives != tests {
+		t.Errorf("Should have %d - got %d", tests, receives)
+	}
+	p.Quit()
+
 }
